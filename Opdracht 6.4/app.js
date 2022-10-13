a46a85853e224d2bcecc6a042eedbd60278845fe
SofieTCR <113505920+SofieTCR@users.noreply.github.com>
2022-10-13 14:09:04 +0200

    Opdracht 6.4

---
+++ Opdracht 6.4/app.js
@@ -0,0 +1,18 @@
+function calculate(par1, par2) {
+    let result = (par1 + par2);
+    console.log(result);
+}
+
+function multiplyByFive(par) {
+    let result = (par * 5);
+    console.log(result);
+}
+
+function calculateMinutes(par) {
+    let result = (par / 60);
+    console.log(result);
+}
+
+calculate(7, 12);
+multiplyByFive(10);
+calculateMinutes(120);
