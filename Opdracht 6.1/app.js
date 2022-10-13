28d461ff1a96dd6b070b476c2ed5f8ff74cd16df
SofieTCR <113505920+SofieTCR@users.noreply.github.com>
2022-10-13 11:49:23 +0200

    Opdracht 6.1

---
+++ Opdracht 6.1/app.js
@@ -0,0 +1,8 @@
+function kleur_aanpassen() {
+    var HeadingToAdjust = document.getElementById("TestHeader2");
+    HeadingToAdjust.style.color = "Orange";
+    HeadingToAdjust.innerHTML = "Inhoud aangepast";
+}
+
+
+kleur_aanpassen();
